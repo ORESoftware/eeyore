50649476a89611be91082016604a5f908e040283
Alexander Mills <alexander.mills@autodesk.com>
2016-11-02 20:33:53 -0700

    temp

--- every.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by t_millal on 10/21/16.
- */
-
-
-
-console.log(['a','a'].every(function(){
-    return false;
-}));
--- test/test-src/par-vs-series/par-vs-series-1.test.js
+++ test/test-src/par-vs-series/par-vs-series-1.test.js
@@ -2,107 +2,101 @@ const suman = require('suman');
 const Test = suman.init(module, {});
 
 
-
-Test.describe('1', {mode: 'series'}, function (example_dot_com,timeout) {
-
-    //////////
-
-    console.log('timeout =>',timeout);
+Test.describe('1', {mode: 'series'}, function (example_dot_com, timeout, it) {
 
 
-    this.it.cb('one', t => {
+    it.cb('one', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('two', t => {
+    it.cb('two', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('three', t => {
+    it.cb('three', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('four', t => {
+    it.cb('four', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('five', t => {
+    it.cb('five', t => {
         setTimeout(t.done, 1000);
     });
 
 });
 
 
-Test.describe('2', {mode: 'series'}, function (example_dot_com) {
+Test.describe('2', {mode: 'series'}, function (example_dot_com, it) {
 
-    //////
-    this.it.cb('one', t => {
+
+    it.cb('one', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('two', t => {
+    it.cb('two', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('three', t => {
+    it.cb('three', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('four', t => {
+    it.cb('four', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('five', t => {
+    it.cb('five', t => {
         setTimeout(t.done, 1000);
     });
 
 });
 
-Test.describe('3', {mode: 'series'}, function (example_dot_com, timeout) {
+Test.describe('3', {mode: 'series'}, function (example_dot_com, timeout, it) {
 
     ////
-    this.it.cb('one', t => {
+    it.cb('one', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('two', t => {
+    it.cb('two', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('three', t => {
+    it.cb('three', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('four', t => {
+    it.cb('four', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('five', t => {
+    it.cb('five', t => {
         setTimeout(t.done, 1000);
     });
 
 });
 
-Test.describe('4', {mode: 'series'}, function (example_dot_com) {
+Test.describe('4', {mode: 'series'}, function (example_dot_com, it) {
 
-    //////
-    this.it.cb('one', t => {
+    it.cb('one', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('two', t => {
+    it.cb('two', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('three', t => {
+    it.cb('three', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('four', t => {
+    it.cb('four', t => {
         setTimeout(t.done, 1000);
     });
 
-    this.it.cb('five', t => {
+    it.cb('five', t => {
         setTimeout(t.done, 1000);
     });
 
